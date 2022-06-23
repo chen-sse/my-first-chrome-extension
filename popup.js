ae8fd94812dd199c4c7620b8d3f6d46b7dea32ba
Andrew Chen <andyschen2013@gmail.com>
2022-06-22 21:53:31 -0400

    Implemented popup.js and added comments

--- popup.js
+++ popup.js
@@ -11,3 +11,27 @@ let changeColor = document.getElementById("changeColor");
 chrome.storage.sync.get("color", ({ color }) => {
     changeColor.style.backgroundColor = color;
 });
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+    // query() returns a Promise, and await returns the fulfilled value of this Promise, which is presumably an array of Tabs
+    // let [tab] is an example of array destructuring (ES6): take the first element of returned array and assign it to variable 'tab'
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    // inject script into target context (current tab)
+    // parameter is ScriptInjection object with 'target' and 'function' properties
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        // setPageBackgroundColor can be used here due to JS hoisting
+        function: setPageBackgroundColor,
+    });
+});
+
+// The body of this function will be executed as a content script inside the current page
+function setPageBackgroundColor() {
+    chrome.storage.sync.get("color", ({ color }) => {
+        // document.body returns the <body> element
+        // .style.backgroundColor allows us to access the CSS background-color property of the HTML element (inline style attribute of element)
+        document.body.style.backgroundColor = color;
+    });
+}
