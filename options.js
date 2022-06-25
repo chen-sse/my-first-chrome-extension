a4d3ff8ccae268a44b76c2b8aa5ba41541ea90a8
Andrew Chen <andyschen2013@gmail.com>
2022-06-25 01:21:52 -0400

    Added comments to button.css and options.js

--- options.js
+++ options.js
@@ -1,45 +1,58 @@
 let page = document.getElementById("buttonDiv");
 let selectedClassName = "current";
+// 'const' means you cannot change the memory location that's pointed to (no reassignment, no redeclaration)
 const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
 
-// Reacts to a button click by marking the selected butotn and saving the selection
+// reacts to a button click by marking the selected button and saving the selection
+// this is the event listener callback--it accepts a single parameter, an Event object describing the event that has occurred
 function handleButtonClick(event) {
-    // Remove styling from the previously selected color
+    // remove styling from the previously selected color
+    // access target property of Event object, i.e. the HTML element that fires the event
+    // event.target.parentElement is 'page' (buttonDiv) in this case
+    // in this case, querySelector's parameter is a class selector
     let current = event.target.parentElement.querySelector(
         `.${selectedClassName}`
     );
+    // current is false iff current = null (all objects are true, and null is the only non-object return value)
+    // if selected button exists and is not current button, unmark previously selected button
     if (current && current !== event.target) {
+        // Element.classList is a property that returns a DOMTokenList collection of the class attributes of the element
+        // remove() method of DOMTokenList interface/object removes specified tokens from list
         current.classList.remove(selectedClassName);
     }
 
-    // Mark the button as selected
+    // mark the button as selected and store color of button
     let color = event.target.dataset.color;
     event.target.classList.add(selectedClassName);
     chrome.storage.sync.set({ color });
 }
 
-// Add a button to the page for each supplied color
+// add a button to the page for each supplied color
 function constructOptions(buttonColors) {
+    // get() presumably returns object with 'color' property equal to value of key 'color'
     chrome.storage.sync.get("color", (data) => {
         let currentColor = data.color;
-        // For each color we were provided...
+        // for each color we were provided...
         for (let buttonColor of buttonColors) {
-            // ... create a button with that color...
+            // first, create new DOM element (button)
             let button = document.createElement("button");
+            // set 'color' attribute--'color' is a standard HTML attribute
             button.dataset.color = buttonColor;
             button.style.backgroundColor = buttonColor;
 
-            // ... mark the currently selected color...
+            // mark currently selected color
             if (buttonColor == currentColor) {
                 button.classList.add(selectedClassName);
             }
 
-            // ... add register a listener for when that button is clicked
+            // register a listener for when that button is clicked
             button.addEventListener("click", handleButtonClick);
+            // then, attach DOM element (button) to DOM tree
+            // <button> has default CSS 'display' property 'inline-block'
             page.appendChild(button);
         }
     });
 }
 
-// Initialize the page by constructing the color options
+// initialize the page by constructing the color options
 constructOptions(presetButtonColors);
